81bef308e25dd36f496e3370999a37b96a2ada4f
MelihYuvaci <melihyuvaci@icloud.com>
2022-09-23 17:28:15 +0300

    Initial Commit

---
+++ WhatsappCloneSwiftUI/WhatsappCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsappCloneSwiftUIApp.swift
+//  WhatsappCloneSwiftUI
+//
+//  Created by Melih Yuvacı on 23.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct WhatsappCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
